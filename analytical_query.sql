c0c0b514bafb3cdf86b0ee9228422101d7a41470
Tommaso Bruzzese <bruzzese.tommaso2005@gmail.com>
2026-03-02 12:18:02 +0100

    convertito sqlite in postgress con docker e aggiurnato gitignore

--- analytical_query.sql
+++ analytical_query.sql
@@ -15,7 +15,7 @@ WITH StatisticheReparti AS (
         departments.department_id,
         departments.department AS nome_reparto,
         COUNT(order_details.product_id) AS totale_prodotti_venduti,
-        ROUND(CAST(SUM(order_details.reordered) AS FLOAT) / COUNT(order_details.product_id) * 100, 2) AS percentuale_riordino
+        ROUND(CAST(SUM(order_details.reordered) AS NUMERIC) / COUNT(order_details.product_id) * 100, 2) AS percentuale_riordino
     FROM departments
     JOIN products ON departments.department_id = products.department_id
     JOIN order_details ON products.product_id = order_details.product_id
@@ -39,5 +39,6 @@ SELECT
     StatisticheReparti.percentuale_riordino,
     OreDiPunta.order_hour_of_day AS ora_di_punta_vendite
 FROM StatisticheReparti
-JOIN OreDiPunta ON StatisticheReparti.department_id = OreDiPunta.department_id AND OreDiPunta.num_riga = 1
+JOIN OreDiPunta ON StatisticheReparti.department_id = OreDiPunta.department_id
+WHERE OreDiPunta.num_riga = 1
 ORDER BY StatisticheReparti.totale_prodotti_venduti DESC;
--- load.py
+++ load.py
@@ -1,77 +1,79 @@
 import pandas as pd
-import sqlite3
+import psycopg2
+import os
+
+cartella_corrente = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(cartella_corrente, 'instacart.csv')
 
 def load_data():
-    csv_file = 'instacart.csv'
-    db_file = 'instacart.db'
     
-    #leggiamo il csv
-    df = pd.read_csv(csv_file)
+    # leggiamo il csv
+    df = pd.read_csv(file)
     
-    #pulizia dati
+    # pulizia dati
     #rimuoviamo duplicate (per intero) o con valori mancanti per garantire 
     #l'integrità dei dati grezzi prima della scomposizione.
     df = df.drop_duplicates()
     df = df.dropna()
     
-
     #trasformiamo il file CSV (denormalizzato) in tabelle relazionali (3NF).
     #usiamo drop_duplicates(subset=[...]) sulle chiavi primarie per estrarre 
     #solo le entità uniche (es. un singolo record per ogni prodotto) ed evitare 
-    # la duplicazione delle primary key nel database.
-
+    #la duplicazione delle primary key nel database.
     users = df[['user_id']].drop_duplicates()
-    departments = df[['department_id',
-                        'department']].drop_duplicates(subset=['department_id'])
-    aisles = df[['aisle_id',
-                    'aisle']].drop_duplicates(subset=['aisle_id'])
-    products = df[['product_id',
-                    'product_name',
-                    'aisle_id',
-                    'department_id']].drop_duplicates()
-    orders = df[['order_id',
-                    'user_id',
-                    'eval_set',
-                    'order_number',
-                    'order_dow',
-                    'order_hour_of_day',
-                    'days_since_prior_order']].drop_duplicates()
-    order_details = df[['order_id',
-                        'product_id',
-                        'add_to_cart_order',
-                        'reordered']].drop_duplicates()
+    departments = df[['department_id', 'department']].drop_duplicates(subset=['department_id'])
+    aisles = df[['aisle_id', 'aisle']].drop_duplicates(subset=['aisle_id'])
+    products = df[['product_id', 'product_name', 'aisle_id', 'department_id']].drop_duplicates()
+    orders = df[['order_id', 'user_id', 'eval_set', 'order_number', 'order_dow', 'order_hour_of_day', 'days_since_prior_order']].drop_duplicates()
+    order_details = df[['order_id', 'product_id', 'add_to_cart_order', 'reordered']].drop_duplicates()
 
-    #connessione al db
-    conn = sqlite3.connect(db_file)
-    cursor = conn.cursor()
-    
-    #svuotiamo le tabelle esistenti prima del caricamento. 
-    #questo ci permette di eseguirlo 
-    #più volte senza rischiare di duplicare i dati nel database.
+    conn = None
     try:
-        cursor.execute("DELETE FROM order_details;")
-        cursor.execute("DELETE FROM orders;")
-        cursor.execute("DELETE FROM products;")
-        cursor.execute("DELETE FROM aisles;")
-        cursor.execute("DELETE FROM departments;")
-        cursor.execute("DELETE FROM users;")
-        conn.commit()
-
-        #caricamento dei dati nelle tabelle corrispondenti
-        users.to_sql('users', conn, if_exists='append', index=False)
-        departments.to_sql('departments', conn, if_exists='append', index=False)
-        aisles.to_sql('aisles', conn, if_exists='append', index=False)
-        products.to_sql('products', conn, if_exists='append', index=False)
-        orders.to_sql('orders', conn, if_exists='append', index=False)
-        order_details.to_sql('order_details', conn, if_exists='append', index=False)
+        #connessione al db su Docker
+        conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="instacart", 
+            user="postgres",
+            password="admin"
+        )
+        cursor = conn.cursor()
         
-        print("caricamento completato")
+        #svuotiamo le tabelle esistenti prima del caricamento. 
+        #questo ci permette di eseguirlo più volte senza rischiare di duplicare i dati nel database.
+        cursor.execute("TRUNCATE TABLE order_details, orders, products, aisles, departments, users CASCADE;")
+        
+        #caricamento dei dati nelle tabelle corrispondenti trasformando i df in liste
+        cursor.executemany("INSERT INTO users (user_id) VALUES (%s)", 
+                            users.values.tolist())
+        
+        cursor.executemany("INSERT INTO departments (department_id, department) VALUES (%s, %s)", 
+                            departments.values.tolist())
+        
+        cursor.executemany("INSERT INTO aisles (aisle_id, aisle) VALUES (%s, %s)", 
+                            aisles.values.tolist())
+        
+        cursor.executemany("INSERT INTO products (product_id, product_name, aisle_id, department_id) VALUES (%s, %s, %s, %s)", 
+                            products.values.tolist())
+        
+        cursor.executemany("INSERT INTO orders (order_id, user_id, eval_set, order_number, order_dow, order_hour_of_day, days_since_prior_order) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+                            orders.values.tolist())
+        
+        cursor.executemany("INSERT INTO order_details (order_id, product_id, add_to_cart_order, reordered) VALUES (%s, %s, %s, %s)", 
+                            order_details.values.tolist())
+        
+        conn.commit()
+        print("db caricato")
         
     except Exception as errore:
-        print(repr(errore))
+        if conn:
+            conn.rollback()
+        print(errore)
         
     finally:
-        conn.close()
+        if conn:
+            cursor.close()
+            conn.close()
 
 #test funzione
 if __name__ == "__main__":
